c5cb7e547bbecdf9c5f0729f644ed2092ebf6403
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-01-14 18:46:22 -0800

    added slash

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use((req, res) => {
+app.use("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
