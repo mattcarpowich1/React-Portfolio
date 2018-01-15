fd723afdc97b5703382b528f8aff39912ee47f15
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-01-14 16:30:08 -0800

    come on

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/app/build/')));
+app.use(express.static(path.join(__dirname, 'app/build/')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/build/index.html'));
+  res.sendFile(path.join(__dirname, '../app/build/index.html'));
 });
 
 app.listen(process.env.PORT ?
